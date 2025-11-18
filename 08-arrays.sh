5b468135ba907f0d4b982d12acf216eea03c84ae
rajendra <akrajendra597@.com>
2025-11-18 09:58:33 +0530

    01shellscript

---
+++ 08-arrays.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MOPVIES=("HELLO" "AKHIL" "AHENT")
+
+# index starts from  0, size is 3
+
+echo "firest movie: ${MOVIES[0]}"
+echo "second movie: ${MOVIES[1]}"
+echo "third movie: ${MOVIES[2]}"
+
+echo "all movies are: ${MOVIES[@]}"
