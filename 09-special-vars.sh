84bf1d51c82c15c2987170a4fa4a2924e344a228
rajendra <akrajendra597@.com>
2025-11-18 10:07:57 +0530

    01shellscript

---
+++ 09-special-vars.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "all variables passed: $@"
+echo "number of variables: $#"
+echo "script name: $0"
+echo "present working dirctort: $PWD"
+echo "home directory of current user: $Home"
+echo "which user is running this script: $USER"
+echo "process id of current script: $$
+echo "process id of last command in baground: $!
+
+
