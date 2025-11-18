30129a286f3856260b7b409e0cbf87e8acad4992
rajendra <akrajendra597@.com>
2025-11-18 10:10:31 +0530

    01shellscript

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -7,6 +7,9 @@ echo "present working dirctort: $PWD"
 echo "home directory of current user: $Home"
 echo "which user is running this script: $USER"
 echo "process id of current script: $$
+
+sleep 60 &
+
 echo "process id of last command in baground: $!
 
 
