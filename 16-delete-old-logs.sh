c8f8a4f644017194054767a36fc19e2915d335bd
rajendra <akrajendra597@.com>
2025-11-20 08:48:57 +0530

    expense -shell

--- 16-delete-old-logs.sh
+++ 16-delete-old-logs.sh
@@ -31,7 +31,7 @@ CHECK_ROOT(){
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -make +14)
+FILES_TO_DELETE=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files to be deleted: $FILES_TO_DELETE"
 
 while read -r filepath # here filepath is the variable name , you can give any name 
@@ -39,4 +39,4 @@ do
     echo "Deleting file: $filepath" &>>LOG_FILE_NAME
     rm -rf $filepath
     echo "Deleted file: $filepath"
-done <<< $FILES_TO _DELETE
+done <<< $FILES_TO_DELETE
